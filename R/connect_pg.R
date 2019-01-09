3bcc99ba67cb053a2c97dee668dd3ecb02011d96
[Eugene Joh] <[ejoh90@gmail.com]>
2019-01-09 18:13:15 -0500

    ... param to connect_pg

--- R/connect_pg.R
+++ R/connect_pg.R
@@ -1,20 +1,34 @@
-#' Title
+#' Connect to Postgres Database
 #'
 #' @param local
 #' @param getenv
+#' @param ...
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#'
+#' connect_pg(local = FALSE, getenv = FALSE,
+#'   host = "34.15.631",
+#'   port = 5432,
+#'   dbname = "mydb",
+#'   user = "myusername",
+#'   password = "mypw"
+#' )
+
 connect_pg <- function(
   local = FALSE, #connect to local postgres if TRUE
-  getenv = TRUE #connect using credentials in .REnviron file
-  ) {
+  getenv = FALSE, #connect using credentials in .REnviron file
+  ...) {
+
+  if (!getenv) {
+    RPostgres::dbConnect(drv = DBI::dbDriver("Postgres"), ...)
+  }
 
   if (getenv) {
     RPostgres::dbConnect(
-      drv,
+      drv = DBI::dbDriver("Postgres"),
       host = Sys.getenv("db_ip"),
       port = 5432, dbname = Sys.getenv("db_name"),
       user = Sys.getenv("db_user"),
