f411ae7572e20be4d2b415e89bf91c59fc6bc961
[Eugene Joh] <[ejoh90@gmail.com]>
2019-01-10 09:28:43 -0500

    modified file name to write_pgtable

--- R/write_pgtable.R
+++ R/write_pgtable.R
@@ -10,7 +10,7 @@
 #' @export
 #'
 #' @examples
-write_pg <- function(
+write_pgtable <- function(
   input, #value to write to dbWriteTable
   field.types = NULL, #field types to use in dbWriteTable (this will be an required input)
   conn = NULL, #connection to database
