bf32fc4eac34203448c3dcad94ca9803ff9e5a91
[Eugene Joh] <ejoh@bu.edu>
2019-02-08 17:27:34 -0500

    debugged comment table and column feature

--- R/write_pgtable.R
+++ R/write_pgtable.R
@@ -18,6 +18,7 @@
 #' @param tbl_name a required option if \code{nchar_df} argument is a single \code{data.frame}
 #' @param tbl.comments an optional argument to include a comment for the table being written, if a \code{list} it must be named
 #' @param field.comments an optional argument to include comments for each field type within the table being written , if a \code{list} it must be named
+#' @param clean_vars an optional \code{logical} argument that automatically cleans field names to be compatible with Postgres (removes upper-case and periods)
 #' @param ... other arguments passed to \code{\link{dbWriteTable}()}.
 #'
 #' @return \code{write_pgtable()} returns \code{TRUE} invisibly.
@@ -46,10 +47,49 @@ write_pgtable <- function(
   tbl_name = NULL,
   tbl.comments = NULL,
   field.comments = NULL,
+  clean_vars = FALSE,
   ...
   ) {
 
+  .all_to_lower <- function(x) {
+    if (! is.null(names(x)))
+      names(x) <- tolower(names(x))
+    if (is.list(x) & ! is.data.frame(x))
+      x <- lapply(x, function(z) {
+        z <- .all_to_lower(z)
+        names(z) <- gsub("\\.", "_", names(z))
+        return(z)
+      })
+    x
+  }
+
+
+  if (clean_vars) {
+    if (inherits(input, "list")) {
+      input <- .all_to_lower(input)
+      tbl.comments <- gsub("\\.", "_", tolower(tbl.comments))
+      field.comments <- lapply(field.comments, function(x) {
+        setNames(x, gsub("\\.", "_", tolower(names(x))))
+      })
+    }
+
+    if (inherits(input, "data.frame")) {
+      names(input) <- tolower(colnames(input))
+      names(input) <- gsub("\\.", "_", colnames(input))
+    }
+
+
+  }
+
+
   if (inherits(input, "list")) {
+    if (any(grepl("^[[:upper:]]+$", unlist(lapply(input, colnames))))) {
+      stop("coerce field names to lower case")
+    }
+    if (any(grepl("\\.+", unlist(lapply(input, colnames))))) {
+      stop("remove periods in field names")
+    }
+
     purrr::map(names(input), function(tab) {
       DBI::dbWriteTable(
         conn = conn,
@@ -61,19 +101,19 @@ write_pgtable <- function(
       message(paste0("WRITE TABLE for ", tab, " completed"))
     })
 
-    if (exists(tbl.comments)) {
+    if (!missing(tbl.comments)) {
       # add dimension checks
       tbl_cl <- purrr::map(names(input), function(tab) {
         DBI::sqlInterpolate(conn = conn,
                             sql = "COMMENT ON TABLE ?schema.?table IS ?comment",
-                            schema = DBI::dbQuoteIdentifier(conn = conn,
-                                                            x = DBI::SQL(schema)),
-                            table = DBI::dbQuoteIdentifier(conn = conn,
-                                                           x = DBI::SQL(tab)),
+                            schema = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(schema)),
+                            table = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(tab)),
                             comment = tbl.comments[[tab]])
-        })
+      })
 
-      purrr::map(unlist(tbl_cl), function(comment) {
+
+
+      purrr::map(tbl_cl, function(comment) {
         qry <- DBI::dbSendQuery(
           conn = conn,
           statement = comment)
@@ -81,19 +121,16 @@ write_pgtable <- function(
         DBI::dbClearResult(qry)})
     }
 
-    if (exists(field.comments)) {
-      # add dimenion checks
+    if (!missing(field.comments)) {
+      # add dimension checks
       field_cl <- purrr::map(names(input), function(tab) {
-      purrr::map_chr(names(field.comments[[tab]]), function(fields) {
+      purrr::map_chr(names(field.comments[[tab]]), function(field) {
         DBI::sqlInterpolate(conn = conn,
                             sql = "COMMENT ON COLUMN ?schema.?table.?fields IS ?comment",
-                            schema = DBI::dbQuoteIdentifier(conn = conn,
-                                                            x = DBI::SQL(schema)),
-                            table = DBI::dbQuoteIdentifier(conn = conn,
-                                                           x = DBI::SQL(tab)),
-                            fields = DBI::dbQuoteIdentifier(conn = conn,
-                                                            x = DBI::SQL(fields)),
-                            comment = field.comments[[tab]][[fields]])
+                            schema = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(schema)),
+                            table = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(tab)),
+                            fields = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(field)),
+                            comment = field.comments[[tab]][[field]])
       })
     })
 
@@ -109,6 +146,13 @@ write_pgtable <- function(
   }
 
   if (inherits(input, "data.frame")) {
+    if (any(grepl("^[[:upper:]]+$", names(input)))) {
+      stop("coerce field names to lower case")
+    }
+    if (any(grepl("^\\.+$", names(input)))) {
+      stop("remove periods in field names")
+    }
+
     if (missing(tbl_name)) tbl_name <- deparse(substitute(input))
     DBI::dbWriteTable(
       conn = conn,
@@ -119,17 +163,14 @@ write_pgtable <- function(
       ...)
     message(paste0("WRITE TABLE for ", tbl_name, " completed"))
 
-    if (exists(tbl.comments)) {
+    if (!missing(tbl.comments)) {
       if (!is.character(tbl.comments)) stop("tbl.comments must be character")
 
       tbl_cl <- DBI::sqlInterpolate(conn = conn,
                           sql = "COMMENT ON TABLE ?schema.?table IS ?comment",
-                          schema = DBI::dbQuoteIdentifier(conn = conn,
-                                                          x = DBI::SQL(schema)),
-                          table = DBI::dbQuoteIdentifier(conn = conn,
-                                                         x = DBI::SQL(tbl_name)),
-                          comment = DBI::dbQuoteIdentifier(conn = conn,
-                                                           tbl.comments))
+                          schema = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(schema)),
+                          table = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(tbl_name)),
+                          comment = tbl.comments)
 
       qry <- DBI::dbSendQuery(
           conn = conn,
@@ -141,19 +182,16 @@ write_pgtable <- function(
 
     }
 
-    if (exists(field.comments)) {
+    if (!missing(field.comments)) {
       # add dimension checks, must be same as ncol(input)
 
-      field_cl <- purrr::map_char(names(field.comments), function(fields) {
+      field_cl <- purrr::map_chr(names(field.comments), function(field) {
         DBI::sqlInterpolate(conn = conn,
                             sql = "COMMENT ON COLUMN ?schema.?table.?fields IS ?comment",
-                            schema = DBI::dbQuoteIdentifier(conn = conn,
-                                                            x = DBI::SQL(schema)),
-                            table = DBI::dbQuoteIdentifier(conn = conn,
-                                                           x = DBI::SQL(tbl_name)),
-                            fields = DBI::dbQuoteIdentifier(conn = conn,
-                                                            x = DBI::SQL(fields)),
-                            comment = field.comments[[fields]])
+                            schema = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(schema)),
+                            table = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(tbl_name)),
+                            fields = DBI::dbQuoteIdentifier(conn = conn, x = DBI::SQL(field)),
+                            comment = field.comments[[field]])
       })
 
       purrr::map(unlist(field_cl), function(comment) {
