a007ea0a05d10203bbfddde030afe86a6f9ffeba
[Eugene Joh] <ejoh@bu.edu>
2019-02-08 13:49:06 -0500

    added PG comment on SQL

--- R/write_pgtable.R
+++ R/write_pgtable.R
@@ -8,11 +8,16 @@
 #' This function is essentially a wrapper for \code{\link{dbWriteTable}()}. See details in \code{RPostgres}
 #' documentation under \code{postgres-tables}.
 #'
+#' If \code{input} is a \code{list} of data frames, dimensions of \code{field.types}, \code{tbl.comments}, and
+#' \code{field.comments} must match \code{input}.
+#'
 #' @param input a \code{data.frame} or \code{list} of data frames.
 #' @param field.types a named \code{character} vector or a named \code{list} of named \code{character} vectors.
 #' @param conn a object inheriting from \code{DBIDriver} or \code{DBIConnection}.
 #' @param schema an optional argument to specify the desired database schema location, default is \code{public}
-#' @param tbl_name a require option if \code{nchar_df} argument is a \code{data.frame}
+#' @param tbl_name a required option if \code{nchar_df} argument is a single \code{data.frame}
+#' @param tbl.comments an optional argument to include a comment for the table being written, if a \code{list} it must be named
+#' @param field.comments an optional argument to include comments for each field type within the table being written , if a \code{list} it must be named
 #' @param ... other arguments passed to \code{\link{dbWriteTable}()}.
 #'
 #' @return \code{write_pgtable()} returns \code{TRUE} invisibly.
@@ -39,6 +44,8 @@ write_pgtable <- function(
   conn = NULL,
   schema = "public",
   tbl_name = NULL,
+  tbl.comments = NULL,
+  field.comments = NULL,
   ...
   ) {
 
@@ -54,6 +61,50 @@ write_pgtable <- function(
       message(paste0("WRITE TABLE for ", tab, " completed"))
     })
 
+    if (exists(tbl.comments)) {
+      # add dimension checks
+      tbl_cl <- purrr::map(names(input), function(tab) {
+        DBI::sqlInterpolate(conn = conn,
+                            sql = "COMMENT ON TABLE ?schema.?table IS ?comment",
+                            schema = DBI::dbQuoteIdentifier(conn = conn,
+                                                            x = DBI::SQL(schema)),
+                            table = DBI::dbQuoteIdentifier(conn = conn,
+                                                           x = DBI::SQL(tab)),
+                            comment = tbl.comments[[tab]])
+        })
+
+      purrr::map(unlist(tbl_cl), function(comment) {
+        qry <- DBI::dbSendQuery(
+          conn = conn,
+          statement = comment)
+        qry
+        DBI::dbClearResult(qry)})
+    }
+
+    if (exists(field.comments)) {
+      # add dimenion checks
+      field_cl <- purrr::map(names(input), function(tab) {
+      purrr::map_chr(names(field.comments[[tab]]), function(fields) {
+        DBI::sqlInterpolate(conn = conn,
+                            sql = "COMMENT ON COLUMN ?schema.?table.?fields IS ?comment",
+                            schema = DBI::dbQuoteIdentifier(conn = conn,
+                                                            x = DBI::SQL(schema)),
+                            table = DBI::dbQuoteIdentifier(conn = conn,
+                                                           x = DBI::SQL(tab)),
+                            fields = DBI::dbQuoteIdentifier(conn = conn,
+                                                            x = DBI::SQL(fields)),
+                            comment = field.comments[[tab]][[fields]])
+      })
+    })
+
+    purrr::map(unlist(field_cl), function(comment) {
+      qry <- DBI::dbSendQuery(
+        conn = conn,
+        statement = comment)
+      qry
+      DBI::dbClearResult(qry)})
+    }
+
 
   }
 
@@ -68,5 +119,51 @@ write_pgtable <- function(
       ...)
     message(paste0("WRITE TABLE for ", tbl_name, " completed"))
 
-  }
+    if (exists(tbl.comments)) {
+      if (!is.character(tbl.comments)) stop("tbl.comments must be character")
+
+      tbl_cl <- DBI::sqlInterpolate(conn = conn,
+                          sql = "COMMENT ON TABLE ?schema.?table IS ?comment",
+                          schema = DBI::dbQuoteIdentifier(conn = conn,
+                                                          x = DBI::SQL(schema)),
+                          table = DBI::dbQuoteIdentifier(conn = conn,
+                                                         x = DBI::SQL(tbl_name)),
+                          comment = DBI::dbQuoteIdentifier(conn = conn,
+                                                           tbl.comments))
+
+      qry <- DBI::dbSendQuery(
+          conn = conn,
+          statement = tbl_cl)
+      qry
+      DBI::dbClearResult(qry)
+
+
+
+    }
+
+    if (exists(field.comments)) {
+      # add dimension checks, must be same as ncol(input)
+
+      field_cl <- purrr::map_char(names(field.comments), function(fields) {
+        DBI::sqlInterpolate(conn = conn,
+                            sql = "COMMENT ON COLUMN ?schema.?table.?fields IS ?comment",
+                            schema = DBI::dbQuoteIdentifier(conn = conn,
+                                                            x = DBI::SQL(schema)),
+                            table = DBI::dbQuoteIdentifier(conn = conn,
+                                                           x = DBI::SQL(tbl_name)),
+                            fields = DBI::dbQuoteIdentifier(conn = conn,
+                                                            x = DBI::SQL(fields)),
+                            comment = field.comments[[fields]])
+      })
+
+      purrr::map(unlist(field_cl), function(comment) {
+        qry <- DBI::dbSendQuery(
+          conn = conn,
+          statement = comment)
+        qry
+        DBI::dbClearResult(qry)})
+      }
+
+    }
+
 }
